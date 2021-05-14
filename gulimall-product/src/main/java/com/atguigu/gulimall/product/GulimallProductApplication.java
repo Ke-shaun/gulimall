a381d10cd7d7b46eb9d459f978d89c8ed104f5b0
keshaun <837063032@qq.com>
2021-05-14 08:10:04 +0800

    项目初始化

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -2,7 +2,9 @@ package com.atguigu.gulimall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ *  产品服务启动类
+ * */
 @SpringBootApplication
 public class GulimallProductApplication {
 
